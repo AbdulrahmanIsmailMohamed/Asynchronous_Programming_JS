cfca719e3b562c403c3edac8544ba7b81a6b4ea3
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-01-11 18:17:53 +0200

    promisse

---
+++ promisse.js
@@ -0,0 +1,12 @@
+/**
+* *steps to make ice cream by promisse
+*/
+
+let stocks = {
+    Fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"]
+};
+
+// time & work
